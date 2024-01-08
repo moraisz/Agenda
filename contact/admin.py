6bdfcc4d4b2ec8be730f0713c6f38046978d50a2
Morais <marcosviniciusm99@gmail.com>
2024-01-08 16:14:18 -0300

    feat: add front-end

--- contact/admin.py
+++ contact/admin.py
@@ -5,7 +5,8 @@ from contact import models
 
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
-    list_display = ('id', 'first_name', 'last_name', 'phone',)
+    list_display = ('id', 'first_name', 'last_name', 'phone', 'show')
+    list_editable = 'first_name', 'last_name', 'show',
     ordering = ('id',)
     search_fields = ('id', 'first_name', 'last_name', 'phone',)
     list_per_page = 10
--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,8 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+
+from contact.models import Contact
 
 
 def index(request):
+    contacts = Contact.objects \
+        .filter(show=True)\
+        .order_by('id')[:10]
+
+    context = {
+        'contacts': contacts,
+        'site_title': 'Contatos - ',
+    }
+
     return render(
         request,
         'contact/index.html',
+        context
+    )
+
+
+def contact(request, contact_id):
+    single_contact = get_object_or_404(
+        Contact,
+        pk=contact_id,
+        show=True,
+    )
+
+    contact_name = f'{single_contact.first_name} {single_contact.last_name} - '
+
+    context = {
+        'contact': single_contact,
+        'site_title': contact_name,
+    }
+
+    return render(
+        request,
+        'contact/contact.html',
+        context
     )

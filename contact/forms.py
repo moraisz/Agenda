39b36f3e1841a356dbb26418d1e23ee0a2918bac
Morais <marcosviniciusm99@gmail.com>
2024-01-29 13:44:30 -0300

    add forms.py

---
+++ contact/forms.py
@@ -0,0 +1,42 @@
+from django import forms
+from django.core.exceptions import ValidationError
+
+from . import models
+
+
+class ContactForm(forms.ModelForm):
+    first_name = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                'class': 'classe-a classe-b',
+                'placeholder': 'Aqui veio do init',
+            }
+        ),
+        label='Primeiro Nome',
+        help_text='Texto de ajuda para seu usuário',
+    )
+
+    class Meta:
+        model = models.Contact  # type: ignore
+        fields = (
+            'first_name', 'last_name', 'phone',
+        )
+
+    def clean(self):
+        # cleaned_data = self.cleaned_data
+
+        self.add_error(
+            'first_name',
+            ValidationError(
+                'Mensagem de erro',
+                code='invalid'
+            )
+        )
+        self.add_error(
+            'first_name',
+            ValidationError(
+                'Mensagem de erro 2',
+                code='invalid'
+            )
+        )
+        return super().clean()

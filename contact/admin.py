446b568e6c9901df2e165d08b8f7e90f071debdd
Morais <marcosviniciusm99@gmail.com>
2024-10-21 16:05:34 -0300

    feat: add category model

--- contact/admin.py
+++ contact/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from contact.models import Contact
+from contact import models
 
-@admin.register(Contact)
+@admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
     list_display = 'id', 'first_name', 'last_name', 'phone',
     ordering = '-id',
@@ -11,3 +11,8 @@ class ContactAdmin(admin.ModelAdmin):
     list_max_show_all = 200
     list_editable = 'first_name', 'last_name',
     list_display_links = 'id', 'phone'
+
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = 'name',
+    ordering = '-id',

e5c79a319fc2e2edc4dea6ee79d2ffa3e9aa1934
Morais <marcosviniciusm99@gmail.com>
2024-10-31 23:30:23 -0300

    feat: search field functional

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.db.models import Q
 from contact.models import Contact
 from django.http import Http404
 
@@ -35,5 +36,32 @@ def contact(request, contact_id):
     return render(
         request,
         'contact/contact.html',
+        context,
+    )
+
+def search(request):
+    search_value = request.GET.get('q', '')
+    
+    if search_value == '':
+        return redirect('contact:index')
+
+    contacts = Contact.objects\
+        .filter(show=True)\
+        .filter(
+            Q(first_name__icontains=search_value) |
+            Q(last_name__icontains=search_value) |
+            Q(phone__icontains=search_value) |
+            Q(email__icontains=search_value)
+        )\
+        .order_by('-id')
+
+    context = {
+        'contacts': contacts,
+        'site_title': 'Search - '
+    }
+
+    return render(
+        request,
+        'contact/index.html',
         context,
     )
